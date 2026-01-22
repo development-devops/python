844618abbc7a56ee426b16126b9e2bfedbc5dd31
namitchp <namitchp@gmail.com>
2026-01-22 20:18:00 +0530

    working on condition

--- condition.py
+++ condition.py
@@ -1,11 +1,22 @@
 age=int(input('enter your age:'));
 is_member=True;
 
-if age>=18:
-    if is_member:
-        print("You are eligible for voting with member discount.");
-        print(2+4 )
-    else:
-        print("You are eligible for voting without member discount.");
+# if age>=18:
+#     if is_member:
+#         print("You are eligible for voting with member discount.");
+#         print(2+4 )
+#     else:
+#         print("You are eligible for voting without member discount.");
+# else:
+#     print("You are not eligible for voting.");
+
+
+
+if age<18:
+    print('hello your age less than 18');
+elif age>=30:
+    print("your gage is greater than 30")
+elif age>=50:
+    print("your age is greater than 50")
 else:
-    print("You are not eligible for voting.");
+    print("your age is greater then 18")
