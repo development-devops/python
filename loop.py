a157d6fb14e1fb3d2cc22a53f43562d2297fa758
namitchp <namitchp@gmail.com>
2026-01-22 21:13:24 +0530

    working on loop

---
+++ loop.py
@@ -0,0 +1,32 @@
+# n=6;
+# n= int(input("input want to  number:"))
+# for i in range(0,n):
+#     print(i)
+
+
+
+# array
+# array=["orange","banana","Apple"];
+
+# for i in array:
+#     print(i ,"hello")
+
+# # topple
+
+#     topple=("orange","banana","Apple");
+
+# for i in array:
+#     print(i ,"hello")
+
+
+# object ={3,8,9,"orange"}
+# for o in object:
+#     print("o",o)
+
+
+
+# fruiteList=["apple","Mango","banana"];
+
+# for index in range(len(fruiteList)):
+
+#     print("index of :" ,fruiteList[index] ,"is" ,index )
